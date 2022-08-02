5a6814aff6242e83fd2e2e253154b1276cab89ef
Arturo Quirós <joseq2408@gmail.com>
2022-08-02 17:52:01 -0600

    fixes on nav

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,28 +1,21 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-
-import { UIContext } from '../../context/ui';
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import { UIContext } from "../../context/ui";
 
 export const Navbar = () => {
-
-  const { openSideMenu } = useContext( UIContext );
-
+  const { openSideMenu } = useContext(UIContext);
 
   return (
-      <AppBar position='sticky'>
-          <Toolbar>
-              <IconButton 
-                size='large'
-                edge="start"
-                onClick={ openSideMenu }
-              >
-                  <MenuOutlinedIcon />
-              </IconButton>
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton size="large" edge="start" onClick={openSideMenu}>
+          <MenuOutlinedIcon />
+        </IconButton>
 
-              <Typography variant='h6'>OpenJira</Typography>
-          </Toolbar>
-      </AppBar>
-  )
+        <Typography variant="h6">OpenJira</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
