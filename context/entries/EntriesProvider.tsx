957feac63c1469517e4d028cc574ebfc52822994
Arturo Quirós <joseq2408@gmail.com>
2022-08-05 18:47:36 -0600

    entries context + uuid

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,17 +1,36 @@
 import { FC, PropsWithChildren, useReducer } from "react";
+import { Entry } from "../../interfaces";
 import { EntriesContext, entriesReducer } from "./";
+import { v4 as uuidv4 } from "uuid";
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
 const initalState: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description: "Terminar el cuso de Next",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description: "Terminar el cuso de React Avanzado",
+      status: "in-progress",
+      createdAt: Date.now() - 1,
+    },
+    {
+      _id: uuidv4(),
+      description: "Terminar el cuso de Node",
+      status: "finished",
+      createdAt: Date.now() - 2,
+    },
+  ],
 };
 
-export const EntriesProvider: FC<PropsWithChildren<EntriesState>> = ({
-  children,
-}) => {
+export const EntriesProvider: FC = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, initalState);
 
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-import { lightTheme, darkTheme } from '../themes';
-import { UIProvider } from '../context/ui';
-
-
-
+import { lightTheme, darkTheme } from "../themes";
+import { UIProvider } from "../context/ui";
+import { EntriesProvider } from "../context/entries";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UIProvider>
-      <ThemeProvider theme={ darkTheme }>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </UIProvider>
-  )
+    <EntriesProvider>
+      <UIProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
+    </EntriesProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
