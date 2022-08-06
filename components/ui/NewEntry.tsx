b353b9628289aa546549bf43e3d5fbf001ef236f
Arturo Quirós <joseq2408@gmail.com>
2022-08-05 21:43:14 -0600

    add almost done (context remaining)

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,9 +1,43 @@
+import { ChangeEvent, useContext, useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import { EntriesProvider } from "../../context/entries/EntriesProvider";
+import { EntriesContext } from "../../context/entries/EntriesContext";
 
 export const NewEntry = () => {
+  //-------------------------------------------
+  const { addNewEntry } = useContext(EntriesContext);
+
+  //-------------------------------------------
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [touched, setTouched] = useState(false);
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSave = () => {
+    if (inputValue.trim().length === 0) {
+      setTouched(true);
+      return;
+    }
+
+    addNewEntry(inputValue);
+    setTouched(false);
+    setIsAdding(false);
+    setInputValue("");
+  };
+
+  const onCancel = () => {
+    setTouched(false);
+    setIsAdding(false);
+    setInputValue("");
+  };
+  //----------------------------------------------
+
   return (
     <Box
       sx={{
@@ -11,43 +45,63 @@ export const NewEntry = () => {
         paddingX: 1,
       }}
     >
-      <Button
-        fullWidth
-        variant="contained"
-        color="secondary"
-        startIcon={<AddOutlinedIcon />}
-      >
-        New Entry
-      </Button>
+      {isAdding ? (
+        <>
+          <TextField
+            value={inputValue}
+            onChange={onInputChange}
+            label="New Entry"
+            error={inputValue.trim().length <= 0 && touched}
+            helperText={
+              inputValue.trim().length <= 0 && touched
+                ? "Please check your entry"
+                : ""
+            }
+            onBlur={() => setTouched(true)}
+            fullWidth
+            autoFocus
+            multiline
+            sx={{
+              marginTop: 2,
+              marginBottom: 1,
+            }}
+          ></TextField>
 
-      <TextField
-        label="New Entry"
-        helperText="Check the Entry"
-        fullWidth
-        autoFocus
-        multiline
-        sx={{
-          marginTop: 2,
-          marginBottom: 1,
-        }}
-      ></TextField>
+          <Box display="flex" justifyContent="space-between">
+            <Button
+              variant="outlined"
+              color="secondary"
+              startIcon={<CancelOutlinedIcon />}
+              onClick={onCancel}
+            >
+              Cancel
+            </Button>
 
-      <Box display="flex" justifyContent="space-between">
-        <Button
-          variant="outlined"
-          color="secondary"
-          startIcon={<CancelOutlinedIcon />}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          startIcon={<SaveOutlinedIcon />}
-        >
-          Save
-        </Button>
-      </Box>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<SaveOutlinedIcon />}
+              onClick={onSave}
+            >
+              Save
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            startIcon={<AddOutlinedIcon />}
+            onClick={() => {
+              setIsAdding(true);
+            }}
+          >
+            New Entry
+          </Button>
+        </>
+      )}
     </Box>
   );
 };
