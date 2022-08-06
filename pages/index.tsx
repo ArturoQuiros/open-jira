c5792c5e2e681dd786e0240e4be619b3dafcaee1
Arturo Quirós <joseq2408@gmail.com>
2022-08-05 18:11:35 -0600

    card p1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import { Typography } from '@mui/material';
-import type { NextPage } from 'next';
-import { Layout } from '../components/layouts';
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import type { NextPage } from "next";
+import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    <Layout title="Dashboard">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: "calc(100vh - 100px)",
+            }}
+          >
+            <CardHeader title="Pending" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
-  )
-}
+  );
+};
 
 export default HomePage;
